f0475848ad46d992b8db73c4bdf57371002c0bd8
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-03-01 16:39:19 +0530

    minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CameraCropperComponent } from './camera/camera.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CameraCropperComponent, CommonModule],
+  imports: [CameraCropperComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  open: boolean = false;
-  title = 'cropper';
-  toggleCamera(): void {
-    this.open = !this.open;
-    console.log('Camera cropper open:', this.open);
-  }
-}
+export class AppComponent {}
