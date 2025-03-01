05b1f5f6cd895c79c093e3a4dcebe9ff5e55de83
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-03-01 14:56:08 +0530

    removed dirts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PostComponent } from './posts/posts.component';
 
 export const routes: Routes = [];
